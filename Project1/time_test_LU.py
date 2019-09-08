d742bcbfcd2ed432cd52bb8ad0b6be5939d89bcc
martimc <martin_cars@hotmail.com>
2019-09-08 17:27:00 +0200

    Update time_test_LU.py

--- Project1/time_test_LU.py
+++ Project1/time_test_LU.py
@@ -1,16 +1,37 @@
 from LU import pylib
 import numpy as np
+import time
 
-N = 5
-A = np.zeros((N,N))
-A[0,0] = 2; A[0,1] = -1; A[-1,-1] = 2; A[-1,-2] = -1
+def f_func(x):
+    '''
+    calculating the source term of the poisson equation
+    '''
+    sol = 100*np.exp(-10*x)
+    return sol
 
-for i in range(1, N-1):
-    for j in range(1, N-1):
-        if i == j:
-            A[i, j] = 2
-            A[i, j+1] = -1
-            A[i, j-1] = -1
+n_max = 4
+i = np.linspace(1, n_max, n_max)
+N = 10**i
+N1 = 10**5
+h = 1/(N+1)
+for i in range(len(N)):
+    n = int(N[i])
+    x = np.linspace(0,1,n+2)
+    A = np.zeros((n, n))
+    A[0,0] = 2; A[0,1] = -1; A[-1,-1] = 2; A[-1,-2] = -1
+    b = f_func(x[1:-1])*(h[i]**2)
 
-pl = pylib()
-print(pl.luDecomp(A))
+    for i in range(1, n-1):
+        for j in range(1, n-1):
+            if i == j:
+                A[i, j] = 2
+                A[i, j+1] = -1
+                A[i, j-1] = -1
+
+    start = time.time()
+    pl = pylib()
+    LU, index, d = pl.luDecomp(A)
+    u = pl.luBackSubst(LU, index, b)
+    end = time.time()
+
+    print('time taken to compute the LU-decomposition and the solution is %.4f seconds with n = %g' % ((end-start), n))
